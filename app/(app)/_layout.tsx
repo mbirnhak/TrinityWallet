b375be198fbb9acc91305df5525456d8208bf37e
Shivanshu <shivanshu.dwivedi@trincoll.edu>
2025-01-03 12:18:39 -0500

    Adding the changes for signout

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Redirect, Stack, useRouter } from "expo-router";
 import { useAuth } from "@/context/AuthContext";
 import { useEffect } from "react";
+import { View, Text } from 'react-native';
 
 export default function ProtectedLayout() {
   const { authState, isLoading } = useAuth();
@@ -8,9 +9,10 @@ export default function ProtectedLayout() {
 
   if (isLoading) {
     return (
-      < div style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <p>Loading...</p>
-      </div >
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Loading...</Text>
+        </View>
+
     );
   }
 
